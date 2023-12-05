b244345d166574c07f577874a8f7b9bc51b10d06
De1fy <103572164+cegou121@users.noreply.github.com>
2023-12-05 19:13:07 +0200

    Update ChristmasPlugin.java
    create commands off snow))

--- ChristmasPlugin.java
+++ ChristmasPlugin.java
@@ -1,64 +1,51 @@
-package org.example.ua.itristan.christmasplugin;
+package org.example.ua.itristan.christmasplugiт
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.World;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ChristmasPlugin extends JavaPlugin implements Listener {
 
+    private boolean snowEffectEnabled = true;
+
     @Override
     public void onEnable() {
-        // Регистрируем нашего слушателя
-        getServer().getPluginManager().registerEvents(this, this);
+        Bukkit.getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (cmd.getName().equalsIgnoreCase("snow")) {
-            startSnowfall();
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                snowFallEffect(player.getLocation());
+            } else {
+                sender.sendMessage("This command can only be executed by a player.");
+            }
+            return true;
+        } else if (cmd.getName().equalsIgnoreCase("snows")) {
+            snowEffectEnabled = !snowEffectEnabled;
+            sender.sendMessage("Snow effect " + (snowEffectEnabled ? "enabled" : "disabled"));
             return true;
         }
         return false;
     }
 
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        // Запуск анимации падения снега при присоединении новых игроков (если активна)
-        if (isSnowfallActive()) {
-            startSnowfall(event.getPlayer().getWorld());
+    public void onPlayerMove(PlayerMoveEvent event) {
+        if (snowEffectEnabled) {
+            snowFallEffect(event.getPlayer().getLocation());
         }
     }
 
-    private void startSnowfall() {
-        // Запуск анимации падения снега для всех миров на сервере
-        for (World world : Bukkit.getWorlds()) {
-            startSnowfall(world);
-        }
-    }
-
-    private void startSnowfall(World world) {
-        Location snowfallLocation = getRandomLocation(world);
-        world.spawnEntity(snowfallLocation, EntityType.DROPPED_ITEM);
-    }
-
-    private boolean isSnowfallActive() {
-        // Здесь можно добавить проверки, чтобы определить, является ли снег уже активным
-        // Это может быть в виде переменной состояния плагина или других условий
-        return true;
-    }
-
-    private Location getRandomLocation(World world) {
-        // Генерируем случайную позицию для снега
-        int x = (int) (Math.random() * 100);
-        int z = (int) (Math.random() * 100);
-        int y = world.getHighestBlockYAt(x, z);
-        return new Location(world, x, y, z);
+    private void snowFallEffect(Location location) {
+        location.getWorld().spawnFallingBlock(location, Material.SNOW, (byte) 0);
     }
 }
